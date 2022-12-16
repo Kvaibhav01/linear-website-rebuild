c4daf01128db6bdbb43b8cd397a12ab860153cfc
Vaibhav <khulbevaibhavdev@gmail.com>
2022-12-16 18:39:15 +0530

    Added the updated button component ✨

--- components/button.tsx
+++ components/button.tsx
@@ -9,14 +9,19 @@ interface ButtonProps extends VariantProps<typeof buttonClasses> {
 const buttonClasses = cva('rounded-full inline-flex items-center', {
   variants: {
     variant: {
-      primary: 'bg-primary-gradient hover:text-shadow hover:shadow-primary',
-      secondary: '',
-      tertiary: '',
+      primary: [
+        'bg-primary-gradient hover:text-shadow hover:shadow-primary transition-[shadow, text-shadow]',
+        '[&_.icon-wrapper]:ml-2',
+      ],
+      secondary: [
+        'text-offWhite bg-white bg-opacity-10 border border-transparent-white backdrop-filter-[12px] hover:bg-opacity-20 transition-colors ease-in',
+        '[&_.icon-wrapper]:bg-transparent-white [&_.icon-wrapper]:rounded-full [&_.icon-wrapper]:px-2 [&_.icon-wrapper]:ml-2 [&_.icon-wrapper]:-mr-2',
+      ],
     },
     size: {
       small: 'text-xs px-3 h-7',
-      medium: 'text-sm px-4 h-8',
-      large: 'text-md px-6 h-12',
+      medium: 'text-md px-4 h-8',
+      large: 'text-lg px-6 h-12',
     },
     defaultVariants: {
       variant: 'primary',
@@ -24,6 +29,10 @@ const buttonClasses = cva('rounded-full inline-flex items-center', {
     },
   },
 });
+
+export const IconWrapper = ({ children }: { children: React.ReactNode }) => (
+  <span className='icon-wrapper'>{children}</span>
+);
 
 export const Button = ({ children, href, variant, size }: ButtonProps) => {
   return (
